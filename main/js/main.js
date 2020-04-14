a4c1fca67105c8b010c13bc095a945cd452618bc
liangDaDa2020.github.io <36720300+liangDaDa2020@users.noreply.github.com>
2020-04-14 09:25:13 +0800

    侧边导肮

--- main/js/main.js
+++ main/js/main.js
@@ -24,49 +24,62 @@ $(function(){
 	}
 	
 })*/
-//侧边菜单
+//侧边菜单start
 $(function(){
-	var navItemObj=$("#side_nav div p")
-	var contentObj=$(".con_bar");
-	var contentItemObj=$(".con_bar .inner");
-	var itemHeightArr=[];
-	contentItemObj.each(function(key,index){
+	var indexAside=new Asideright({
+		aside:"#side_nav",
+		content:"#side_content"
+	})
+	indexAside.init();
+})
+function Asideright(obj){
+	this.aside=$(obj.aside);
+	this.content=$(obj.content);
+	this.navItemObj=this.aside.find(".sideItem");
+	this.contentItemObj=this.content.find(".inner");
+	this.itemHeightArr=[];
+	this.indexOn=0;
+}
+Asideright.prototype.init=function(){
+	var This=this;
+	this.contentItemObj.each(function(key,index){
 		var height=$(this).offset().top;
 		height=height-50;
-		itemHeightArr[key]=height;
+		This.itemHeightArr[key]=height;
 	})
-	function move(indexOn){
-		$(window).unbind("scroll")
-		var height=itemHeightArr[indexOn];
-		$('html, body').stop().animate({ scrollTop:height }, 'slow',function(){
-			var scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
-			$(window).bind('scroll', function(){
-				getAnchor()
-			});
-		});
-	}
-	function getAnchor(){
-		var scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
-		var indexOn=0;
-		for(var i=0;i<itemHeightArr.length;i++){
-			if(scrollTop>=itemHeightArr[i] && scrollTop<itemHeightArr[i+1]){
-				indexOn=i;
-			}else if(scrollTop>=itemHeightArr[i] && i==0){
-				indexOn=0;
-			}else if(scrollTop>=itemHeightArr[i] && !itemHeightArr[i+1]){
-				indexOn=i;
-			}
-		}
-		navItemObj.eq(indexOn).addClass("nav-itemOn").siblings().removeClass("nav-itemOn");
-	}
-	
-	navItemObj.click(function(){
-		$(this).addClass("nav-itemOn").siblings().removeClass("nav-itemOn");
-		var indexOn=$(this).index();
-		move(indexOn)
-	})
-	getAnchor()
+	this.getAnchor()
 	$(window).bind('scroll', function(){
-		getAnchor()
+		this.getAnchor()
 	});
-})
+	
+	this.navItemObj.click(function(){
+		$(this).addClass("nav-itemOn").siblings().removeClass("nav-itemOn");
+		This.indexOn=$(this).index();
+		This.move(This.indexOn)
+	})
+}
+Asideright.prototype.move=function(){
+	var This=this;
+	$(window).unbind("scroll")
+	var height=this.itemHeightArr[this.indexOn];
+	$('html, body').stop().animate({ scrollTop:height }, 'slow',function(){
+		var scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+		$(window).bind('scroll', function(){
+			This.getAnchor()
+		});
+	});
+}
+Asideright.prototype.getAnchor=function(){
+	var scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+	for(var i=0;i<this.itemHeightArr.length;i++){
+		if(scrollTop>=this.itemHeightArr[i] && scrollTop<this.itemHeightArr[i+1]){
+			this.indexOn=i;
+		}else if(scrollTop>=this.itemHeightArr[i] && i==0){
+			this.indexOn=0;
+		}else if(scrollTop>=this.itemHeightArr[i] && !this.itemHeightArr[i+1]){
+			this.indexOn=i;
+		}
+	}
+	this.navItemObj.eq(this.indexOn).addClass("nav-itemOn").siblings().removeClass("nav-itemOn");
+}
+//侧边菜单end
